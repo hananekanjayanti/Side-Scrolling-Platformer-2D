93a2f569db7b7ba2756810cb6799e0b8879e3b91
Hana Nining Eka Nur Jayanti <hananiningeka69@gmail.com>
2022-07-12 15:43:37 +0700

    tambah animasi jump

--- side scrolling 2d/Assets/Scripts/PlayerController.cs
+++ side scrolling 2d/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
 
     private Animator anim;
+
+     private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,27 +27,41 @@ public class PlayerController : MonoBehaviour
         body.velocity = new Vector2 (horizontalInput * speed, body.velocity.y); 
 
         //flip gambar char
-        if(horizontalInput > 0.001f)
+        if(horizontalInput > 0.01f)
         {
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(1,1,1);
         }
-        else if (horizontalInput < 0.001f)
+        else if (horizontalInput < -0.01f) //kalau belum input apa2 berarti karakter 0 dan 0<0.01 jadi flip
         {
             transform.localScale = new Vector3(-1,1,1);
         }
 
-
-
         //karaketer gerak vertikal
 
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && grounded) 
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            Jump();
         }
 
         // set anim run
         anim.SetBool("run", horizontalInput !=0 );
+        anim.SetBool("grounded", grounded);
 
-        
+    }
+       
+    private void Jump()
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+            anim.SetTrigger("jump");
+            grounded = false;
+
+        }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
